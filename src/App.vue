b68bfe7c2a81277ad403fb8a4fabc2cc6fc07bc7
bndstocco <bndst77@gmail.com>
2022-10-07 04:04:46 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
-<Navbar />
  <Corpo />
- <Footer />
 </template>
 
 <script>
-import Navbar from '../src/components/Navbar.vue'
 import Corpo from '../src/components/Corpo.vue'
-import Footer from '../src/components/Footer.vue'
 export default {
     name: 'Home',
     components: {
-    Navbar,
-    Corpo,
-    Footer,
+    Corpo
   }
 }
 
